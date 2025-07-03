e653912599c5dfbd6a35b14ea2b31d914612bd67
fatemeh-khajeh <72465357+fatemeh-khajeh@users.noreply.github.com>
2025-07-03 10:29:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,61 @@ chain = prompt | model
 # Streamlit UI 
 st.set_page_config(page_title= "🏫 Alexandra- prof de Teccart", layout="centered")
 st.title("👩🏻‍🏫 Alexandra, Prof de Teccart")
+
+# session state initilization
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+if "doc_text" not in st.session_state:
+    st.session_state.doc_text = ""
+
+with st.sidebar:
+    st.header("Charger un document")
+    uploaded = st.file_uploader("PDF ou DOCX :", type=["pdf","docx"])
+    if uploaded:
+        ext = uploaded.name.lower().split(".")[-1]
+        if ext == "pdf" :
+            from pypdf import PdfReader
+            reader = PdfReader(uploaded)
+            text = "\n".join(p.extract_text() or "" for p in reader.pages)
+        if ext == "docx":
+            import docx
+            doc = docx.Document(uploaded)
+            text = "\n".join(p.text for p in doc.paragraphs)
+        st.session_state.doc_text = text.strip()[:20_000]
+        st.success(" Document charge! Vous pouvez poser les questions.")
+        # print("text",text)
+                
+# Afficher l'historique
+
+for speaker , msg in st.session_state.chat_history:
+    prefix = "👩🏽‍🎓" if speaker == 'Vous' else "👩🏽‍🏫"
+    st.markdown(f"**{prefix}  {speaker}**:  {msg}")
+    
+user_input = st.chat_input("❓ Pose ta question...") 
+
+
+# Traitment de la question
+if user_input:
+    with st.spinner("Alexandra reflechit ..."):
+        result = chain.invoke({
+            "doc_context" : st.session_state.doc_text or "(aucun document)", 
+            "chat_context" : "\n".join(
+                f"{s}: {m}" for s, m in st.session_state.chat_history),
+            "question" : user_input
+        })
+        
+    # Memoriser l'echange
+    
+    st.session_state.chat_history.append(("Vous", user_input))
+    st.session_state.chat_history.append(("Alexandra", result )) 
+    st.rerun()   
+        
+                     
+                   
+
+
+
+
+
+
+
